744d40511b6926cbeedb0be8358d75f12bf44478
Andrew Palmer <andrew.pa@outlook.com>
2018-02-17 12:09:53 -0800

    splatted points, different intial condition

--- bndb-sim/src/main.rs
+++ bndb-sim/src/main.rs
@@ -92,7 +92,7 @@ impl<'b> Tree<'b> {
             mass_center /= total_mass;
             Tree::Internal {
                 mass_center, total_mass, center, extents, bodies: nodeb,
-                children: chlb.iter().enumerate().map(|(i, x)| x.as_ref().map(|bs| Box::new(Tree::construct(bs, new_octree_center(i, center, extents), extents/2.0)))).collect()
+                children: chlb.par_iter().enumerate().map(|(i, x)| x.as_ref().map(|bs| Box::new(Tree::construct(bs, new_octree_center(i, center, extents), extents/2.0)))).collect()
             }
         }
     }
@@ -107,6 +107,13 @@ impl<'b> Tree<'b> {
             &Tree::Internal { mass_center, total_mass, ref children, extents, .. } => {
                 if extents.x.max(extents.y.max(extents.z)) / body.position.distance(mass_center) > 0.5 {
                     children.iter().map(|oc| oc.as_ref().map_or_else(|| Vec3::zero(), |c| c.calculate_force(body))).sum()
+                    /*let mut s = Vec3::zero();
+                    for c in children {
+                        if let &Some(ref ch) = c {
+                            s += ch.calculate_force(body);
+                        }
+                    }
+                    s*/
                 } else {
                     gravity_between(body, mass_center, total_mass)
                 }
@@ -115,20 +122,56 @@ impl<'b> Tree<'b> {
     }
 }
 
+fn hsv_to_rgb((h,s,v) : (Real,Real,Real)) -> (Real,Real,Real) {
+    let r = (((h * 6.0 - 3.0).abs() - 1.0).min(1.0).max(0.0) - 1.0) * s + 1.0;
+    let g = ((2.0 - (h * 6.0 - 2.0).abs()).min(1.0).max(0.0) - 1.0) * s + 1.0;
+    let b = ((2.0 - (h * 6.0 - 4.0).abs()).min(1.0).max(0.0) - 1.0) * s + 1.0;
+    (r*v,g*v,b*v)
+}
+
 fn render_image(bodies: &Vec<Body>) -> image::RgbImage {
-    let mut img = image::ImageBuffer::new(640, 480);
-    let T = cgmath::perspective(cgmath::Deg(60.0), 640.0 / 480.0, 0.1, 100.0)*
-        cgmath::Matrix4::look_at(cgmath::Point3::new(0.0, 14.0, -30.0), cgmath::Point3::new(0.0, 0.0, 0.0), cgmath::Vector3::unit_y()); 
+    use cgmath::*;
+    let width:i32 = 640;
+    let height:i32 = 480;
+    let size = 8;
+    let T =
+        Matrix4::from_translation(Vector3::new(320.0, 240.0, 0.0))*
+        Matrix4::from_nonuniform_scale(320.0, 240.0, 1.0)*
+        perspective(cgmath::Deg(60.0), (width as Real) / (height as Real), 0.1, 100.0)*
+        Matrix4::look_at(cgmath::Point3::new(15.0, 10.0, -30.0), cgmath::Point3::new(0.0, 0.0, 0.0), cgmath::Vector3::unit_y()); 
+
+    let mut img = image::ImageBuffer::new(width as u32, height as u32);
+    let mut pxs: Vec<(Real,Real,Real)> = Vec::new();
+    pxs.resize_default((width * height) as usize);
+
     for b in bodies {
         let mut p = T * cgmath::Vector4::new(b.position.x, b.position.y, b.position.z, 1.0);
-        p.x /= p.w;
-        p.y /= p.w;
-        p.z /= p.w;
-        if p.x < 0.0 || p.x > img.width() as f32 || p.y < 0.0 || p.y > img.height() as f32 { continue }
-        let p : &mut image::Rgb<u8> = img.get_pixel_mut(p.x as u32, p.y as u32);
-        p.data[0] += 40;
-        p.data[1] += 40;
-        p.data[2] += 40;
+        let x = (p.x / p.w) as i32;
+        let y = (p.y / p.w) as i32;
+        if x < 0 || x >= width || y < 0 || y >= height { continue; }
+        for iy in 0..(size+1) {
+            let dy = (iy as i32 * 2 - size as i32)/2;
+            let v = ((dy as Real) / (size as Real));
+            if (y+dy) < 0 || (y+dy) >= height { continue; }
+            for ix in 0..(size+1) {
+                let dx = (ix as i32 * 2 - size as i32)/2;
+                let u = ((dx as Real) / (size as Real));
+                let idx = ((x+dx) + (y+dy)*(width as i32)) as usize;
+                if idx >= pxs.len() { continue; }
+                let sat = (1.0-(u*u + v*v)) * 0.0015;
+                let (r,g,b) = hsv_to_rgb(( (dot(b.velocity,b.velocity)*10.0).sin()*0.5+0.5 ,0.9,sat));
+                pxs[idx].0 += r;
+                pxs[idx].1 += g;
+                pxs[idx].2 += b;
+            }
+        }
+    }
+
+    for (x, y, px) in img.enumerate_pixels_mut() {
+        let r: u8 = (pxs[(x + y*(width as u32)) as usize].0 * 255.0).min(255.0) as u8;
+        let g: u8 = (pxs[(x + y*(width as u32)) as usize].1 * 255.0).min(255.0) as u8;
+        let b: u8 = (pxs[(x + y*(width as u32)) as usize].2 * 255.0).min(255.0) as u8;
+        *px = image::Rgb([r, g, b]);
     }
     img
 }
@@ -146,7 +189,7 @@ fn main() {
     let start_init = Instant::now();
     let mut bodies = Vec::new();
     let mut rnd = rand::thread_rng();
-    for i in 0..1600 {
+    /*for i in 0..32_000 {
         let theta: Real = rnd.gen::<Real>()*2.0*pi();
         let r: Real = rnd.gen::<Real>()*8.0 + 8.0;
         let y: Real = (rnd.gen::<Real>()*2.0 - 1.0)*0.3;
@@ -155,11 +198,11 @@ fn main() {
             index: i,
             position: p,
             velocity: p.normalize().cross(Vec3::unit_y()) * 0.1,
-            mass: 50.0
+            mass: 25.0
         });
     }
     let bc = bodies.len();
-    for i in 0..1600 {
+    for i in 0..32_000 {
         let theta: Real = rnd.gen::<Real>()*2.0*pi();
         let r: Real = rnd.gen::<Real>()*8.0 + 8.0;
         let y: Real = (rnd.gen::<Real>()*2.0 - 1.0)*0.3;
@@ -168,11 +211,11 @@ fn main() {
             index: bc+i,
             position: p,
             velocity: p.normalize().cross(Vec3::unit_z()) * 0.1,
-            mass: 50.0
+            mass: 25.0
         });
     }
     let bc = bodies.len();
-    for i in 0..1600 {
+    for i in 0..32_000 {
         let theta: Real = rnd.gen::<Real>()*2.0*pi();
         let r: Real = rnd.gen::<Real>()*8.0 + 8.0;
         let y: Real = (rnd.gen::<Real>()*2.0 - 1.0)*0.3;
@@ -181,16 +224,41 @@ fn main() {
             index: bc+i,
             position: p,
             velocity: p.normalize().cross(Vec3::unit_x()) * 0.1,
-            mass: 50.0
+            mass: 25.0
+        });
+    }*/
+    for i in 0..200_000 {
+        let theta: Real = rnd.gen::<Real>()*2.0*pi();
+        let phi: Real = rnd.gen::<Real>()*2.0*pi();
+        let r: Real = rnd.gen::<Real>()*2.0 + 15.0;
+        let p = Vec3::new(r*theta.sin()*phi.cos(), r*theta.sin()*phi.sin(), r*theta.cos());
+        bodies.push(Body {
+            index: i,
+            position: p,
+            velocity: p.normalize().cross(Vec3::unit_y()) * 0.1,
+            mass: 5.0
+        });
+    }
+    let bc = bodies.len();
+    for i in 0..32_000 {
+        let theta: Real = rnd.gen::<Real>()*2.0*pi();
+        let r: Real = rnd.gen::<Real>()*8.0 + 13.0;
+        let y: Real = (rnd.gen::<Real>()*2.0 - 1.0)*0.4;
+        let p = Vec3::new(r*theta.cos(), y, r*theta.sin());
+        bodies.push(Body {
+            index: bc+i,
+            position: p,
+            velocity: p.normalize().cross(Vec3::unit_y()) * -0.2,
+            mass: 8.0
         });
     }
     let bc = bodies.len();
     bodies.push(Body {
-            index: bc+1,
-            position: Vec3::zero(),
-            velocity: Vec3::zero(),
-            mass: 5e9
-        });
+        index: bc+1,
+        position: Vec3::zero(),
+        velocity: Vec3::zero(),
+        mass: 6e9
+    });
     let end_init = Instant::now();
     println!("init took: {:?}", (end_init - start_init));
     let mut next_bodies = Vec::new();
@@ -205,8 +273,8 @@ fn main() {
     //out.start_file("metadata", zipopts).expect("start metadata file");
 
     let simm = SimulationMetadata {
-        delta_time: 0.05,
-        steps: 10_0
+        delta_time: 0.5,
+        steps: 100_000
     };
 
     /*println!("serializing metadata");
@@ -248,8 +316,11 @@ fn main() {
             img.save(&mut std::fs::File::create(&dest_path).unwrap(), image::PNG).unwrap();
             dest_path.pop();
         }
+//        let start_tree_build = Instant::now();
         let tree = Tree::construct(&cb.iter().map(|x| x).collect(), Vec3::zero(), Vec3::new(500.0, 500.0, 500.0));
-        *nb = cb.par_iter().map(|body| {
+ //       let end_tree_build = Instant::now();
+  //      println!("\ttree build took: {:?}", end_tree_build-start_tree_build);
+        cb.par_iter().map(|body| {
             let f = tree.calculate_force(body);
             let a = f / body.mass;
             Body {
@@ -257,7 +328,28 @@ fn main() {
                 velocity: body.velocity + a * dt,
                 .. *body
             }
-        }).collect::<Vec<_>>();
+        }).collect_into(nb);
+        //nb.clear();
+        /*cb.par_iter().map(|body| {
+            let f = tree.calculate_force(body);
+            let a = f / body.mass;
+            Body {
+                position: body.position + body.velocity * dt,
+                velocity: body.velocity + a * dt,
+                .. *body
+            }
+        }).for_each(|b| nb.push(b));*/
+        /*for body in cb {
+            let f = tree.calculate_force(body);
+            let a = f / body.mass;
+            nb.push(Body {
+                position: body.position + body.velocity * dt,
+                velocity: body.velocity + a * dt,
+                .. *body
+            });
+        }*/
+   //     let end_update = Instant::now();
+    //    println!("\tupdate took: {:?}", end_update-end_tree_build);
         if step % 500 == 0 {
             let p = nb.par_iter().map(|b| b.velocity*b.mass).sum();
             println!("system momentum = {:?},\n\tΔp = {:?}", p, last_momentum-p);
